2f739601c8d7c1b19d378cf9efac6ab1cf22ad4f
jorgedq <jorgedq7952@gmail.com>
2022-12-16 00:06:26 -0400

    fix

--- src/main/java/com/chatbot/us/chatbotapi/chatbot/ChatController.java
+++ src/main/java/com/chatbot/us/chatbotapi/chatbot/ChatController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,7 +44,8 @@ public class ChatController {
   }
 
   @DeleteMapping("/{id}")
-  public ResponseEntity<Void> delete(@RequestParam("id") long id) {
+  public ResponseEntity<Void> delete(@PathVariable("id") long id) {
     chatRepository.deleteById(id);
+    return ResponseEntity.noContent().build();
   }
 }
