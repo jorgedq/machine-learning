f4357ed6194eb526002c96c4bdc88dde5127ea04
jorgedq <jorgedq7952@gmail.com>
2022-12-16 00:03:34 -0400

    delete api

--- src/main/java/com/chatbot/us/chatbotapi/chatbot/ChatController.java
+++ src/main/java/com/chatbot/us/chatbotapi/chatbot/ChatController.java
@@ -6,10 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -38,5 +40,10 @@ public class ChatController {
   @PostMapping
   public ResponseEntity<Chat> saveChat(@RequestBody Chat chatModel) {
     return ResponseEntity.ok(chatRepository.save(chatModel));
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@RequestParam("id") long id) {
+    chatRepository.deleteById(id);
   }
 }
